031453c8c4b303cb607a694af0cb8e0ca6296a2a
Ex <exelob25@gmail.com>
2022-02-02 21:49:16 -0300

    updates

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Exequiel Lobaiza Ramos on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
